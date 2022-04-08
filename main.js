bd09ade409257b8af32ff6e003c88c7555e70111
Martin Heywang <martin.heywang1@gmail.com>
2022-04-08 16:39:21 +0200

    Created a basic timeline

--- main.js
+++ main.js
@@ -1,1 +1,43 @@
 console.log("%cWelcome!", "color: skyblue; background-color: #222; font-weight: bold; font-size: 2rem;")
+
+function initializeTimeline() {
+    const timeline = document.querySelector(".story__timeline");
+    const blocksCount = timeline.childElementCount;
+
+    timeline.style.width = `calc(${blocksCount} * 100%)`;
+
+    let timelinePos = 0;
+    const firstPos = 0;
+    const lastPos = blocksCount - 1;
+
+    const previousBtn = document.querySelector(".story__timeline-previous")
+    const nextBtn = document.querySelector(".story__timeline-next")
+
+    const blockWidthPerc = `(100% / ${blocksCount})`;
+
+    previousBtn.addEventListener("click", () => {
+        if(timelinePos === firstPos) return;
+        timelinePos--
+
+        refreshTimelineAndControls()
+    })
+    nextBtn.addEventListener("click", () => {
+        if(timelinePos === lastPos);
+        timelinePos++
+
+        refreshTimelineAndControls()
+    })
+    
+    const refreshTimelineAndControls = () => {
+        timeline.style.transform = `translateX(calc(-${timelinePos} * ${blockWidthPerc}))`
+        previousBtn.disabled = false;
+        nextBtn.disabled = false;
+
+        if(timelinePos === firstPos) previousBtn.disabled = true;
+        if(timelinePos === lastPos) nextBtn.disabled = true;
+    }
+
+    refreshTimelineAndControls();
+}
+
+initializeTimeline()
