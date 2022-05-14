10f9c3c0fe3cf55beedb93174c9d0249608e27da
Martin Heywang <martin.heywang1@gmail.com>
2022-05-14 16:38:10 +0200

    Created basic primary hue slider

--- ts/main.ts
+++ ts/main.ts
@@ -1,5 +1,7 @@
 import * as timeline from "./timeline";
+import * as hueSlider from "./hueSlider";
 
+hueSlider.init();
 timeline.init();
 
 console.log("%cWelcome!", "color: skyblue; background-color: #222; font-weight: bold; font-size: 2rem;")
