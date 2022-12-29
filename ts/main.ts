aa5a153c27881eeb72fb4e732ecee7a4214a4dca
Martin Heywang <martin.heywang1@gmail.com>
2022-12-29 17:20:23 +0100

    Started fresh with a blank project

--- ts/main.ts
+++ ts/main.ts
@@ -1,7 +1,1 @@
-import * as timeline from "./timeline";
-import * as hueSlider from "./hueSlider";
-
-hueSlider.init();
-timeline.init();
-
 console.log("%cWelcome!", "color: skyblue; background-color: #222; font-weight: bold; font-size: 2rem;")
